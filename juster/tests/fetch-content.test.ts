5eea3d4507f590187f3e6922fda532cab1ba8f28
RicardoVercet <ricardobieb01@gmail.com>
2025-10-22 22:49:11 +0530

    test  - getContent, postContent

---
+++ juster/tests/fetch-content.test.ts
@@ -0,0 +1,22 @@
+import { getContent, postContent } from "../core/fetch-content";
+
+describe("Get function", () => {
+    it("fetches content from web", () => {
+        return getContent("https://www.google.com/").then((data) => {
+            expect(data.status).toBe(200);
+        })
+    });
+});
+
+describe("Post function", () => {
+    it("sends post request", () => {
+        const body = JSON.stringify({
+            title: "Hello World",
+            body: "This is a test post",
+            userId: 123,
+        })
+        return postContent("https://jsonplaceholder.typicode.com/posts", body).then((data) => {
+            expect(data.status).toBe(201);
+        });
+    });
+})
