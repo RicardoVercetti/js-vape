2d14aa76777dc74750063e42eb82cdb995e4710c
RicardoVercet <ricardobieb01@gmail.com>
2025-10-23 22:00:57 +0530

    save to file

---
+++ juster/core/store.ts
@@ -0,0 +1,14 @@
+import fs from "fs";
+
+export const saveToCsv = (content: string, filename: string): Promise<boolean> => {
+    return new Promise((resolve, reject) => {
+        fs.writeFile(filename, content, (err) => {
+            if (err) {
+                console.log("error writing to file: " + err);
+                return reject(false);
+            }
+            console.log("file saved successfully!");
+            return resolve(true);
+        });
+    });
+}
